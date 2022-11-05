24bcc83bcb5f5a9c9c451788440413250d12c9a7
Stefan Louie <Stefan.louie@blackbook.ai>
2022-11-05 15:20:49 +1000

    Created navigation component

---
+++ app/Frontend/housing-prediction-frontend/src/app/Features/navigation/navigation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  navbarCollapsed: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
